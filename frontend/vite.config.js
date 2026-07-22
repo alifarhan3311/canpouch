158089c3586e3b6a674fb568cf9bd06544aa2d6f
Rajeel Siddiqui github <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-07-23 02:38:20 +0500

    feat: update backend proxy configuration in Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const backend =
+  'https://canpouch-backend.vercel.app' || 'http://localhost:5001';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5175,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true
-      }
-    }
-  }
+        target: backend,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
